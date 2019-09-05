03b322c834f996200ce3c0683ff92b9665e4a806
XiaoLuo01 <fuxiaoluo01@163.com>
2019-09-05 14:46:06 +0800

    format code & finish password page

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -1,76 +1,82 @@
 <template>
- <div class="login">
+  <div class="login">
     <LoginHeader>
-        <el-form ref="ruleForm" :rules="rules" :model="ruleForm" label-position="left" label-width="0px" slot="container">
-            <div class="title">
-                <h3>账号密码登录</h3>
-            </div>
-            <!-- username -->
-            <el-form-item prop="username">
-                <el-input type="text" v-model="ruleForm.username" auto-complete="off" placeholder="账号">
-                    <i slot="prefix" class="fa fa-user-o"></i>
-                </el-input>
-            </el-form-item>
+      <el-form
+        ref="ruleForm"
+        :rules="rules"
+        :model="ruleForm"
+        label-position="left"
+        label-width="0px"
+        slot="container"
+      >
+        <div class="title">
+          <h3>账号密码登录</h3>
+        </div>
+        <!-- username -->
+        <el-form-item prop="username">
+          <el-input type="text" v-model="ruleForm.username" auto-complete="off" placeholder="账号">
+            <i slot="prefix" class="fa fa-user-o"></i>
+          </el-input>
+        </el-form-item>
 
-            <!-- password -->
-            <el-form-item prop="password">
-                <el-input type="password" v-model="ruleForm.password" auto-complete="off" placeholder="密码">
-                    <i slot="prefix" class="fa fa-lock"></i>
-                </el-input>
-            </el-form-item>
+        <!-- password -->
+        <el-form-item prop="password">
+          <el-input
+            type="password"
+            v-model="ruleForm.password"
+            auto-complete="off"
+            placeholder="密码"
+          >
+            <i slot="prefix" class="fa fa-lock"></i>
+          </el-input>
+        </el-form-item>
 
-            <!-- 登录 button -->
-            <el-form-item>
-                <el-button @click.native.prevent="handleSubmit" type="primary" style="width:100%">
-                    登录
-                </el-button>
-            </el-form-item>
+        <!-- 登录 button -->
+        <el-form-item>
+          <el-button @click.native.prevent="handleSubmit" type="primary" style="width:100%">登录</el-button>
+        </el-form-item>
 
-            <!-- 7天登录和忘记密码 -->
-            <el-form-item>
-                <el-checkbox v-model="ruleForm.autoLogin" :checked="ruleForm.autoLogin">7天内自动登录</el-checkbox>
-                <el-button @click="$router.push('/password')" type="text" class="forget">忘记密码?</el-button>
-            </el-form-item>
-        </el-form>
+        <!-- 7天登录和忘记密码 -->
+        <el-form-item>
+          <el-checkbox v-model="ruleForm.autoLogin" :checked="ruleForm.autoLogin">7天内自动登录</el-checkbox>
+          <el-button @click="$router.push('/password')" type="text" class="forget">忘记密码?</el-button>
+        </el-form-item>
+      </el-form>
     </LoginHeader>
- </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Provide } from "vue-property-decorator";
-import LoginHeader from './LoginHeader.vue'
+import LoginHeader from "./LoginHeader.vue";
 
 @Component({
-    components: { LoginHeader }
+  components: { LoginHeader }
 })
-
 export default class Login extends Vue {
-    @Provide() ruleForm: {
-        username: String;
-        password: String;
-        autoLogin: Boolean;
-    } = {
-        username: '',
-        password: '',
-        autoLogin: true
-    };
+  // 装饰器, 相当于 data return 里面绑定的数据
+  @Provide() ruleForm: {
+    username: String;
+    password: String;
+    autoLogin: Boolean;
+  } = {
+    username: "",
+    password: "",
+    autoLogin: true
+  };
 
-    @Provide() rules = {
-        username: [
-            {required: true, message: '请输入账号', trigger: 'blur'}
-        ],
-        password: [
-            {required: true, message: '请输入密码', trigger: 'blur'}
-        ]
-    }
+  @Provide() rules = {
+    username: [{ required: true, message: "请输入账号", trigger: "blur" }],
+    password: [{ required: true, message: "请输入密码", trigger: "blur" }]
+  };
 
-    handleSubmit():void {
-        (this.$refs['ruleForm'] as any).validate((valid:boolean) => {
-            if (valid) {
-                console.log('校验通过')
-            }
-        })
-    }
+  handleSubmit(): void {
+    (this.$refs["ruleForm"] as any).validate((valid: boolean) => {
+      if (valid) {
+        console.log("校验通过");
+      }
+    });
+  }
 }
 </script>
 
--- src/views/Login/Password.vue
+++ src/views/Login/Password.vue
@@ -1,25 +1,85 @@
 <template>
- <div class="password">
+  <div class="password">
     <LoginHeader>
-        <el-from slot="container">
-            <div class="title">
-                <h3>找回密码</h3>
-            </div>
-        </el-from>
+      <el-from
+        slot="container"
+        :model="ruleForm"
+        :rules="rules"
+        ref="ruleForm"
+        label-position="left"
+        label-width="0px"
+      >
+        <div class="title">
+          <h3>找回密码</h3>
+        </div>
+
+        <!-- username -->
+        <el-form-item prop="username">
+          <el-input type="text" v-model="ruleForm.username" auto-complete="off" placeholder="账号">
+            <i slot="prefix" class="fa fa-user-o"></i>
+          </el-input>
+        </el-form-item>
+
+        <!-- email -->
+        <el-form-item prop="email">
+          <el-input type="text" v-model="ruleForm.email" auto-complete="off" placeholder="邮箱">
+            <i slot="prefix" class="fa fa-envelope-o"></i>
+          </el-input>
+        </el-form-item>
+
+        <!-- 确定 -->
+        <el-form-item>
+          <el-button
+            @click.native.prevent="handleSubmit"
+            :loading="loading"
+            type="primary"
+            style="width:100%;"
+          >确定</el-button>
+        </el-form-item>
+      </el-from>
     </LoginHeader>
- </div>
+  </div>
 </template>
 
-<script>
-import { Component, Vue } from "vue-property-decorator";
-import LoginHeader from './LoginHeader.vue'
+<script lang="ts">
+import { Component, Vue, Provide } from "vue-property-decorator";
+import LoginHeader from "./LoginHeader.vue";
 
 @Component({
-    components: { LoginHeader }
+  components: { LoginHeader }
 })
-
 export default class Password extends Vue {
-    
+  @Provide() ruleForm: {
+    username: String;
+    email: String;
+  } = {
+    username: "",
+    email: ""
+  };
+
+  @Provide() rules = {
+    username: [{ required: true, message: "请输入账号", trigger: "blur" }],
+    email: [
+      {
+        required: true,
+        message: "请输入邮箱地址",
+        trigger: "blur"
+      },
+      {
+        type: "email",
+        message: "请输入正确的邮箱地址",
+        trigger: "blur,change"
+      }
+    ]
+  };
+
+  handleSubmit(): void {
+    (this.$refs["ruleForm"] as any).validate((valid: boolean) => {
+      if (valid) {
+        console.log("校验通过");
+      }
+    });
+  }
 }
 </script>
 
