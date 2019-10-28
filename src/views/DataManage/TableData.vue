c78b4f8692ca102a705a79f10e6afcc95efebc2c
XiaoLuo01 <fuxiaoluo01@163.com>
2019-10-28 15:41:30 +0800

    router config

---
+++ src/views/DataManage/TableData.vue
@@ -0,0 +1,19 @@
+<template>
+ <div class="home">
+    tabledata
+ </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+    components: {}
+})
+
+export default class TableData extends Vue {}
+</script>
+
+<style>
+
+</style>
